330e693b77e45a01941a6a32f9f6e3a5ee0de3ad
Lukasz Went <zakzyne@gmail.com>
2025-09-27 20:36:55 +0200

    test staged files for husky

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,21 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-if [ -z "$husky_skip_init" ]; then
-  readonly husky_skip_init=1
-  export husky_skip_init
-  sh -e "$0" "$@"
-  exit $?
-fi
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-if [ -z "$husky_skip_init" ]; then
-  readonly husky_skip_init=1
-  export husky_skip_init
-  sh -e "$0" "$@"
-  exit $?
-fi
-
-# Shell autocompletion options picked by accident.
-unset -f command_not_found_handle 2>/dev/null
-unset -f _python_argcomplete 2>/dev/null
-
-husky_use_hooks=1
-export husky_use_hooks
+They WILL FAIL in v10.0.0
+"
